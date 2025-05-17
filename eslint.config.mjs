adf8a674857062e124ab6026d49b1ed47e4fb0e3
yevhen-petrunkin <novamovaxxi@gmail.com>
2025-05-17 11:47:55 +0300

    Task#0001 Set up project environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import eslintParser from '@typescript-eslint/parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    eslint.configs.recommended,
+    ...compat.extends(
+        'next/core-web-vitals',
+        'next/typescript',
+        'eslint-config-prettier',
+    ),
+
+    {
+        plugins: {
+            '@typescript-eslint': tseslint,
+        },
+        languageOptions: {
+            parser: eslintParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                project: './tsconfig.json',
+                tsconfigRootDir: __dirname,
+                ecmaFeatures: { jsx: true },
+            },
+        },
+    },
 ];
 
 export default eslintConfig;
